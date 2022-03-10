2d2e2f2beb6f777f2cc770d0635ff6abdf32011f
Alexis Toranzo <alexistoranzoar@gmail.com>
2022-03-10 19:30:16 -0300

    Correct the name of the test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import "@testing-library/jest-dom/extend-expect";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders app", () => {
   const component = render(<App />);
   component.getByText("Consulta de Saldo");
 });
